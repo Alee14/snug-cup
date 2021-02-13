d16550df3d370ce2f2859d72a79c36e8ef0b4a72
Andrew Lee <alee14498@protonmail.com>
2021-02-12 23:35:51 -0500

    Updated page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Container from '@material-ui/core/Container'
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Head>
-        <title>Create Next App</title>
+        <title>Snug Cup Inc.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+     <div className={styles.centre}>
+       <img src="/Project.png" height="15%" width="15%"/>
      <h1>Welcome to the Snug Cup Inc. website!</h1>
-     <p>This website is currently work in progress...</p>
-    </div>
+     </div>
+     <h1>Who are we?</h1>
+     <p>We are a company that inovates cups.</p>
+     <footer className={styles.footer}>
+       <p>&copy; Copyright 2021, Snug Cup Inc. All rights reserved.</p>
+       <p>Made in Next.JS & Hosted on Netlify</p>
+     </footer>
+    </Container>
   )
 }
