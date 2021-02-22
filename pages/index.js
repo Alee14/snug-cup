724bf66fdb46969e382b654c9286b5cf2a14714e
Andrew Lee <alee14498@protonmail.com>
2021-02-22 16:01:28 +0000

    remove image

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,6 @@ export default function Home() {
         <img src="/Project.png" height="17%" width="17%"/>
         <br/>
         <iframe allowFullScreen webkitallowfullscreen width="640" height="480" frameBorder="0" seamless src="https://p3d.in/e/coEzR+spin+load"></iframe>
-        <h2>Original Concept</h2>
-        <img src="/snug_cup_concept.png" width="50%" height="50%" />
         <h2>Render</h2>
         <img src="/snug_cup.png" width="50%" height="50%" />
         <img src="/snug_cup_diagram.png" width="50%" height="50%" />
